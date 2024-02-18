dc8106522f4f9461730acebf29c7134e2497e522
Huijin Yim <152468548+heejinyim@users.noreply.github.com>
2024-02-17 20:14:46 -0600

    Add files via upload

---
+++ AnimalShelter/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AnimalShelter
+{
+    public partial class Form1 : Form
+    {
+        public Customer[] CustomerArray = new Customer[10];
+        public int CustomerArrayIndex = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void CreateCustomer_Click(object sender, EventArgs e)
+        {
+            CustomerArray[CustomerArrayIndex] = new Customer(CusNewLastName.Text, CusNewFirstName.Text, DateTime.Parse(CusNewBirthday.Text));
+            CustomerArray[CustomerArrayIndex].Address = CusNewAddress.Text;
+            CustomerArray[CustomerArrayIndex].Description = CusNewDescription.Text;
+            CustomerList.Items.Add(CustomerArray[CustomerArrayIndex].FirstName);
+
+            CustomerArrayIndex++;
+            
+        }
+
+        public void ShowDetails(Customer cus)
+        {
+            CusFullName.Text = cus.FullName;
+            CusAge.Text = cus.Age.ToString();
+            CusAddress.Text = cus.Address;
+            CusIsQualified.Text = cus.IsQualified.ToString();
+            CusDescription.Text = cus.Description;
+        }
+
+        private void CustomerList_Click(object sender, EventArgs e)
+        {
+            string firstName = CustomerList.SelectedItem.ToString();
+
+            for(int i = 0; i < CustomerArrayIndex; i++)
+            {
+                if (CustomerArray[i].FirstName == firstName)
+                {
+                    ShowDetails(CustomerArray[i]);
+                }
+            }
+        }
+    }
+}
